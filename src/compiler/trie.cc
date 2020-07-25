49ca1ae566e2fbe8d3eb5697a3765d6c175f5268
Romain Foucher <romain.foucher@epita.fr>
2020-07-25 23:37:05 +0200

    trie: remove unused

--- src/compiler/trie.cc
+++ src/compiler/trie.cc
@@ -1,21 +1,19 @@
 #include "trie.hh"
 
 #include <iostream>
-#include <cstdio>
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <cmath>
 
 
-namespace trie {
-
-    void trie_insert(TrieNode *root, const std::string &word) {
+namespace trie
+{
+    void trie_insert(TrieNode *root, const std::string &word)
+    {
         TrieNode *current = root;
-        for (const char &ch : word) {
+        for (const char &ch : word)
+        {
             std::string str = {ch};
             TrieNode *node = current->children[str];
-            if (!node) {
+            if (!node)
+            {
                 node = new TrieNode();
                 current->children[str] = node;
             }
@@ -24,39 +22,42 @@ namespace trie {
         current->end_of_word = true;
     }
 
-    void trie_destroy(struct TrieNode *root) {
-        for (const auto &i : root->children) {
+    void trie_destroy(struct TrieNode *root)
+    {
+        for (const auto &i : root->children)
+        {
             trie::trie_destroy(i.second);
         }
         delete (root);
     }
 
 /* DEBUG */
-
-    void bst_print_dot_aux(const TrieNode *node, std::string cur_str, unsigned &nb) {
+    void bst_print_dot_aux(const TrieNode *node, const std::string &cur_str, unsigned &nb)
+    {
         unsigned i = nb;
         std::cout << "    " << nb << " [label = \"" << cur_str << "\"]\n";
-        for (const auto&[c, child] : node->children) {
+        for (const auto&[c, child] : node->children)
+        {
             ++nb;
             std::cout << "    " << i << " -> " << nb << "\n";
             bst_print_dot_aux(child, c, nb);
         }
     }
 
-    void trie_print(const TrieNode *root) {
+    void trie_print(const TrieNode *root)
+    {
         std::cout << "digraph Trie {\n";
         std::cout << "    node [fontname=\"Arial\"];\n";
 
         if (!root)
             std::cout << "\n";
-        else {
+        else
+        {
             unsigned i = 0;
             bst_print_dot_aux(root, "", i);
         }
 
         std::cout << "}\n";
     }
-
 /* DEBUG END */
-
 }
